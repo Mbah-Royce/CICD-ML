37be98b89b9827fbf112677a441bf40d71379414
Mbah-Royce <alemchu123@yahoo.com>
2026-06-19 00:33:42 +0100

    new changes

--- Makefile
+++ Makefile
@@ -1,1 +1,18 @@
+install:
+    pip install --upgrade pip &&\
+        pip install -r requirements.txt
 
+format:
+    black *.py
+
+train:
+    python train.py
+
+eval:
+    echo "## Model Metrics" > report.md
+    cat ./Results/metrics.txt >> report.md
+
+    echo '\n## Confusion Matrix Plot' >> report.md
+    echo '![Confusion Matrix](./Results/model_results.pnp)' >> report.md
+
+    cml commend create report.md
--- train.py
+++ train.py
@@ -1,1 +1,64 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, f1_score, ConfusionMatrixDisplay, confusion_matrix
+import matplotlib.pyplot as plt
+import skops.io as sio
+from sklearn.ensemble import RandomForestClassifier
+
+drug_df = pd.read_csv("Data/drug.csv")
+drug_df = drug_df.sample(frac=1)
+drug_df.head(3)
+
+# Train Test Split
+X = drug_df.drop("Drug", axis=1).values
+y = drug_df.Drug.values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# sklearn pipeline
+cat_col = [1,2,3]
+num_col = [0,4]
+
+transform = ColumnTransformer(
+    [
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col)
+    ]
+)
+
+pipe = Pipeline(
+    steps = [
+        ("preprocessing", transform),
+        ("model", RandomForestClassifier(n_estimators=100,random_state=42))
+    ]
+)
+pipe.fit(X_train, y_train)
+
+# Model Evaluation
+predictions = pipe.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+f1 = f1_score(y_test, predictions, average="macro")
+
+print("Accuarcy", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1,2))
+
+# with open("Results/metrics.txt", "W") as outfile:
+#     outfile.write(f"\nAccuracy = {accuracy.round(2)}, F1 Score = {f1.round(2)}.")
+
+cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
+disp.plot()
+# plt.savefig("Results/model_results.png", dpi=120)
+
+# Saving Model
+sio.dump(pipe,"Model/drug_pipeline.skops")
+
+
+
+
+
 
